ed457093a6fb3a99102189476f9e55f57bb8db17
Noah Padilla <npizza10@gmail.com>
2021-11-05 21:25:58 -0600

    Updated correct file layout

---
+++ lib/Model/mnist_progress_indicator.dart
@@ -0,0 +1,1 @@
+//was "custom_progress_indicator"
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:digit_classifier/main.dart';
+import 'package:digit_classifier/Controller/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
